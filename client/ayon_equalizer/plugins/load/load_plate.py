c66cb982422c6874874c2810780a57f709fdfaac
Ondřej Samohel <ondrej.samohel@ynput.io>
2024-09-26 23:26:55 +0200

    :bug: fix plate loader

--- client/ayon_equalizer/plugins/load/load_plate.py
+++ client/ayon_equalizer/plugins/load/load_plate.py
@@ -6,27 +6,29 @@ because this workflow is the most common in production.
 If current camera is not defined, it will try to use first camera and
 if there is no camera at all, it will create new one.
 
-TODO:
+TODO (antirotor):
     * Support for setting handles, calculation frame ranges, EXR
       options, etc.
     * Add support for color management - at least put correct gamma
       to image corrections.
 
 """
-import tde4
+import os
+import re
 
 import ayon_core.pipeline.load as load
-from ayon_api import get_version_by_id
-from ayon_equalizer.api import Container, EqualizerHost
+import tde4
 from ayon_core.lib.transcoding import IMAGE_EXTENSIONS
 from ayon_core.pipeline import (
     get_current_project_name,
-    get_representation_context,
+    get_representation_path,
 )
+
+from ayon_equalizer.api import Container, EqualizerHost
 
 
 class LoadPlate(load.LoaderPlugin):
-    families = [
+    product_types = [
         "imagesequence",
         "review",
         "render",
@@ -35,7 +37,7 @@ class LoadPlate(load.LoaderPlugin):
         "online",
     ]
 
-    representations = ["*"]
+    representations = {"*"}
     extensions = {ext.lstrip(".") for ext in IMAGE_EXTENSIONS}
 
     label = "Load sequence"
@@ -44,11 +46,14 @@ class LoadPlate(load.LoaderPlugin):
     color = "orange"
 
     def load(self, context, name=None, namespace=None, options=None):
-        representation = context["representation"]
+        repre_entity = context["representation"]
         project_name = get_current_project_name()
-        version = get_version_by_id(project_name, representation["parent"])
+        version_entity = context["version"]
+        version_attributes = version_entity["attrib"]
+        version_data = version_entity["data"]
 
-        file_path = self.file_path(representation, context)
+        file_path = get_representation_path(repre_entity)
+        file_path = self.format_path(file_path, repre_entity)
 
         camera = tde4.createCamera("SEQUENCE")
         tde4.setCameraName(camera, name)
@@ -62,19 +67,22 @@ class LoadPlate(load.LoaderPlugin):
 
         # set the sequence attributes star/end/step
         tde4.setCameraSequenceAttr(
-            camera, int(version["data"].get("frameStart")),
-            int(version["data"].get("frameEnd")), 1)
+            camera, int(version_attributes.get("frameStart")),
+            int(version_attributes.get("frameEnd")), 1)
 
         container = Container(
             name=name,
             namespace=camera_name,
             loader=self.__class__.__name__,
-            representation=str(representation["_id"]),
+            representation=str(repre_entity["id"]),
         )
         print(container)
         EqualizerHost.get_host().add_container(container)
 
-    def update(self, container, representation):
+    def update(self, container, context):
+        version_entity = context["version"]
+        version_attributes = version_entity["attrib"]
+        repre_entity = context["representation"]
         camera_list = tde4.getCameraList()
         try:
             camera = [
@@ -86,41 +94,45 @@ class LoadPlate(load.LoaderPlugin):
             print(f'Cannot find camera {container["namespace"]}')
             return
 
-        context = get_representation_context(representation)
-        file_path = self.file_path(representation, context)
+        file_path = get_representation_path(repre_entity)
+        file_path = self.format_path(file_path, repre_entity)
 
         # set the path to sequence on the camera
         tde4.setCameraPath(camera, file_path)
 
-        version = get_version_by_id(
-            get_current_project_name(), representation["parent"])
-
         # set the sequence attributes star/end/step
         tde4.setCameraSequenceAttr(
-            camera, int(version["data"].get("frameStart")),
-            int(version["data"].get("frameEnd")), 1)
+            camera, int(version_attributes.get("frameStart")),
+            int(version_attributes.get("frameEnd")), 1)
 
         print(container)
         EqualizerHost.get_host().add_container(container)
 
-    def switch(self, container, representation):
-        self.update(container, representation)
+    def switch(self, container, context):
+        self.update(container, context)
 
-    def file_path(self, representation, context):
-        is_sequence = len(representation["files"]) > 1
-        print(f"is sequence {is_sequence}")
-        if is_sequence:
-            frame = representation["context"]["frame"]
-            hashes = "#" * len(str(frame))
-            if (
-                    "{originalBasename}" in representation["data"]["template"]
-            ):
-                origin_basename = context["originalBasename"]
-                context["originalBasename"] = origin_basename.replace(
-                    frame, hashes
-                )
+    @staticmethod
+    def format_path(path, representation):
+        if not os.path.exists(path):
+            raise RuntimeError("Path does not exist: %s" % path)
 
-            # Replace the frame with the hash in the frame
-            representation["context"]["frame"] = hashes
+        ext = os.path.splitext(path)[-1]
+
+        is_sequence = bool(representation["context"].get("frame"))
+
+        if not is_sequence:
+            filename = path
+        else:
+            hashes = "#" * len(str(representation["context"].get("frame")))
+            filename = re.sub(r"(.*)\.(\d+){}$".format(re.escape(ext)),
+                              "\\1.{}{}".format(hashes, ext),
+                              path)
+
+            filename = os.path.join(path, filename)
+
+        filename = os.path.normpath(filename)
+        filename = filename.replace("\\", "/")
+
+        return filename
 
         return self.filepath_from_context(context)
