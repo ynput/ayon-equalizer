540b536f1bebdd46add76fc287f3928fec92b30e
Ondřej Samohel <ondrej.samohel@ynput.io>
2024-09-27 19:34:16 +0200

    :recycle: refactor to new api

--- client/ayon_equalizer/plugins/create/create_lens_distortion_data.py
+++ client/ayon_equalizer/plugins/create/create_lens_distortion_data.py
@@ -2,10 +2,15 @@ from ayon_equalizer.api import EqualizerCreator
 
 
 class CreateLensDistortionData(EqualizerCreator):
+    """Create lens distortion data."""
+
     identifier = "io.ayon.creators.equalizer.lens_distortion"
     label = "Lens Distortion"
     product_type = "lensDistortion"
     icon = "glasses"
 
-    def create(self, subset_name, instance_data, pre_create_data):
-        super().create(subset_name, instance_data, pre_create_data)
+    def create(
+            self, product_name: str,
+            instance_data: dict, pre_create_data: dict):
+        """Create lens distortion data."""
+        super().create(product_name, instance_data, pre_create_data)
