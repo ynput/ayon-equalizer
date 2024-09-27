8b300e4bc34c077086c3ec544f38afb49192a9b9
Ondřej Samohel <ondrej.samohel@ynput.io>
2024-09-27 19:36:17 +0200

    :art: fix dependencies
    and add `app_host_name`

--- package.py
+++ package.py
@@ -1,9 +1,11 @@
 name = "equalizer"
 title = "3DEqualizer"
 version = "0.1.0-dev.1"
+app_host_name = "equalizer"
 
 client_dir = "ayon_equalizer"
 
+ayon_server_version = ">1.3.0"
 ayon_required_addons = {
     "core": ">=0.3.0",
 }
