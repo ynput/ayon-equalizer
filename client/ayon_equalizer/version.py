ac022c2a2e6d94768ecb686f94d61add582011f0
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2024-10-15 16:31:32 +0200

    bump version to '0.1.0'

--- client/ayon_equalizer/version.py
+++ client/ayon_equalizer/version.py
@@ -1,2 +1,3 @@
-"""Package declaring equalizer addon version."""
-__version__ = "0.1.0-dev.1"
+# -*- coding: utf-8 -*-
+"""Package declaring AYON addon 'equalizer' version."""
+__version__ = "0.1.0"
