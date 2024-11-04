1624af58a8a581f45b1e1551fc3f266ed9c880a7
Joseph HENRY <joseph@welcometolove.tv>
2024-11-04 11:37:54 +0100

    add workfile open hook

---
+++ client/ayon_equalizer/hooks/pre_add_last_workfile_arg.py
@@ -0,0 +1,34 @@
+import os
+
+from ayon_applications import LaunchTypes, PreLaunchHook
+
+
+class AddLast3DEWorkfileToLaunchArgs(PreLaunchHook):
+    """Add last workfile path to launch arguments.
+
+    Copied from ayon_core/hooks/pre_add_last_workfile_arg.py.
+    Checks 'start_last_workfile', if set to False, it will not open last
+    workfile. This property is set explicitly in Launcher.
+    """
+
+    # Execute after workfile template copy
+    order = 10
+    app_groups = ("equalizer", "sdv_3dequalizer")
+    launch_types = LaunchTypes.local
+
+    def execute(self):
+        if not self.data.get("start_last_workfile"):
+            self.log.info("It is set to not start last workfile on start.")
+            return
+
+        last_workfile = self.data.get("last_workfile_path")
+        if not last_workfile:
+            self.log.warning("Last workfile was not collected.")
+            return
+
+        if not os.path.exists(last_workfile):
+            self.log.info("Current context does not have any workfile yet.")
+            return
+
+        # Add path to workfile to arguments
+        self.launch_context.launch_args.extend(["-open", last_workfile])
